115beef79a18bc49ce3b0e58945422fa141e4416
Evgeni Georgiev <evgeni.georgiev@donatix.net>
2025-02-24 09:41:55 +0200

    feature/REG-23
    - cast transaction type to enum

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -34,6 +35,7 @@ class Transaction extends Model
         'coin_id' => 'integer',
         'quantity' => 'float',
         'buy_price' => 'float',
+        'transaction_type' => TransactionTypeEnum::class,
     ];
 
     public function portfolio(): BelongsTo
